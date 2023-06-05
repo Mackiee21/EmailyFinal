49ed13e5f5c8b393383029640adf7aa4058b56f3
Mackiee21 <markpandac02@gmail.com>
2023-06-05 13:40:12 +0800

    fixed spinner on mobile design

--- client/src/Components/MySurveys.jsx
+++ client/src/Components/MySurveys.jsx
@@ -22,20 +22,21 @@ function MySurveys() {
 
         }
     };
-    let className;
     let content;
     if(fetching === 'getting'){
-        content = <div class="d-flex justify-content-center align-items-center">
-                        <div class="spinner-border text-success mr-2" role="status">
-                            <span class="sr-only">Loading...</span>
+        content = <div className='d-flex justify-content-center align-items-center m-0' style={{"min-height": "88vh"}}>
+                    <div className="d-flex justify-content-center align-items-center mt-0">
+                        <div className="spinner-border text-primary mr-2" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                        <span>Fetching data...</span>
                     </div>
-                    <span>Fetching data...</span>
                 </div>
+                
     }else if("okaynabes"){
-        className = 'survey-wrapper';
         content = data?.map((survey, index) => {
                 return( 
-                    <div key={index}>
+                    <div className='survey-wrapper' key={index}>
                         <h1 className='header'>{survey.survey.header}</h1>
                         <p className='about'>{survey.survey.about}</p>
                     </div>
@@ -45,7 +46,7 @@ function MySurveys() {
         content = <h1>ERROR FETCHING DATA</h1>
     }
     return(
-        <div className={className}>{content}</div>
+        <div>{content}</div>
     );
 
 }
